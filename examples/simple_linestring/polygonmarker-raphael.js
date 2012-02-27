d7392374f1ae19b69fb5d07530e98443f3c310ac
Gleb Peregud <gleber.p@gmail.com>
2012-02-27 12:09:58 +0100

    fix polygon maker for mapnet-like coordinates

--- examples/simple_linestring/polygonmarker-raphael.js
+++ examples/simple_linestring/polygonmarker-raphael.js
@@ -14,14 +14,14 @@ com.modestmaps.PolygonMarker = function(map, locations, fillStyle, fillAlpha, st
 
     this.coords = [];
 
-    // top left    
+    // top left
     var maxLat = locations[0].lat;
     var minLon = locations[0].lon;
 
     // bottom right
     var minLat = locations[0].lat;
     var maxLon = locations[0].lon;
-    
+
     for (var i = 0; i < locations.length; i++) {
         this.coords.push(map.provider.locationCoordinate(locations[i]));
         minLat = Math.min(minLat, locations[i].lat);
@@ -30,34 +30,34 @@ com.modestmaps.PolygonMarker = function(map, locations, fillStyle, fillAlpha, st
         maxLon = Math.max(maxLon, locations[i].lon);
     }
 
-    var topLeftLocation = new com.modestmaps.Location(maxLat, minLon);
-    var bottomRightLocation = new com.modestmaps.Location(minLat, maxLon);
+    var topLeftLocation = new com.modestmaps.Location(minLat, minLon);
+    var bottomRightLocation = new com.modestmaps.Location(maxLat, maxLon);
 
 //    console.log(topLeftLocation);
 //    console.log(bottomRightLocation);
-    
+
     this.topLeftCoord = map.provider.locationCoordinate(topLeftLocation);
     this.bottomRightCoord = map.provider.locationCoordinate(bottomRightLocation);
-    
+
 //    console.log(this.topLeftCoord);
 //    console.log(this.bottomRightCoord);
 
 
     // listen for events
-    
-    var follower = this;    
+
+    var follower = this;
     var callback = function(m, a) { return follower.draw(m); };
     map.addCallback('panned', callback);
     map.addCallback('zoomed', callback);
     map.addCallback('centered', callback);
     map.addCallback('extentset', callback);
-    
+
     // get your div on
-    
+
     this.div = document.createElement('div');
     this.div.style.position = 'absolute';
     map.parent.appendChild(this.div);
-    
+
     this.draw(map);
 }
 
@@ -69,13 +69,13 @@ com.modestmaps.PolygonMarker.prototype = {
     coords: null,
     topLeftCoord: null,
     bottomRightCoord: null,
-    
+
     drawZoom: null,
-    
+
     fillStyle: null,
     fillAlpha: null,
     strokeStyle: null,
-    
+
     draw: function(map)
     {
         try {
@@ -85,19 +85,19 @@ com.modestmaps.PolygonMarker.prototype = {
             // too soon?
             return;
         }
-        
+
         /* if(point.x + this.dimensions.x + this.offset.x < 0) {
             // too far left
             this.div.style.display = 'none';
-        
+
         } else if(point.y + this.dimensions.y + this.offset.y < 0) {
             // too far up
             this.div.style.display = 'none';
-        
+
         } else if(point.x + this.offset.x > map.dimensions.x) {
             // too far right
             this.div.style.display = 'none';
-        
+
         } else if(point.y + this.offset.y > map.dimensions.y) {
             // too far down
             this.div.style.display = 'none';
@@ -107,7 +107,7 @@ com.modestmaps.PolygonMarker.prototype = {
             this.div.style.left = point.x + this.offset.x + 'px';
             this.div.style.top = point.y + this.offset.y + 'px';
         } */
-        
+
         this.div.style.display = 'block';
         this.div.style.left = point.x + 'px';
         this.div.style.top = point.y + 'px';
@@ -116,17 +116,17 @@ com.modestmaps.PolygonMarker.prototype = {
 
             var topLeftPoint = map.coordinatePoint(this.topLeftCoord);
             var bottomRightPoint = map.coordinatePoint(this.bottomRightCoord);
-        
+
             var canvasWidth = bottomRightPoint.x - topLeftPoint.x;
             var canvasHeight = bottomRightPoint.y - topLeftPoint.y;
-        
+
             if (this.canvas) {
                 this.canvas.remove();
                 // TODO: resize?
             }
 
             this.canvas = Raphael(this.div, canvasWidth, canvasHeight);
-        
+
             var points = [];
             for (var i = 0; i < this.coords.length; i++) {
                 var point = map.coordinatePoint(this.coords[i]);
@@ -142,7 +142,7 @@ com.modestmaps.PolygonMarker.prototype = {
                 pathParams['fill-opacity'] = this.fillAlpha;
             }
             if (this.strokeStyle) pathParams['stroke'] = this.strokeStyle;
-            
+
             var path = this.canvas.path(pathParams);
 
             path.moveTo(points[0].x, points[0].y);
@@ -150,10 +150,10 @@ com.modestmaps.PolygonMarker.prototype = {
                 path.lineTo(points[i].x, points[i].y);
             }
             path.andClose();
-            
+
             this.drawZoom = map.getZoom();
-        }        
-        
+        }
+
     },
 
     clear: function(){
